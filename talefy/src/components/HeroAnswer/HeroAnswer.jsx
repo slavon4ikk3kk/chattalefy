b8c69209e259032a9ba9160a22650b3ae3ba59c9
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2024-09-28 13:47:33 +0300

    text

--- talefy/src/components/HeroAnswer/HeroAnswer.jsx
+++ talefy/src/components/HeroAnswer/HeroAnswer.jsx
@@ -1,17 +1,31 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import s from "./HeroAnswer.module.css";
-import images from "../../assets/index.js"
+import images from "../../assets/index.js";
+import Typewriter from 'typewriter-effect';
 const HeroAnswer = () => {
-  return (
-    <div className={s.heroAnswer}>
-    <img src={images.pifagor} className={s.heroImg}></img>
-    <div className={s.heroTextArea}>
-      <p className={s.heroName}>Pifagor</p>
-      <p className={s.heroText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ex architecto velit saepe ullam? Ea obcaecati illum magnam asperiores quia?</p>
-      <span className={s.loader}></span>
-    </div>
-  </div>
-  )
+    const [isShowMessage, setIsShowMessage] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsShowMessage(true);
+        }, 3000);
+    }, []);
+    return (
+        <div className={s.heroAnswer}>
+            <img src={images.pifagor} className={s.heroImg}></img>
+            <div className={s.heroTextArea}>
+                <p className={s.heroName}>Pifagor</p>
+                {isShowMessage ? <p className={s.heroText}><Typewriter
+                    options={{
+                        strings: ['Lorem ipsum dolor sit, amet consectetur adipisicing.'],
+                        autoStart: true,
+                        loop: false,
+                    }}
+                /></p> : <span className={s.loader}></span>}
+
+            </div>
+        </div>
+    )
 }
 
 export default HeroAnswer
